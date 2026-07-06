5f3fc6e7d3cb0a4919d78b75f6f885f02f47a381
Andrew Sotiriou <drew.sotiriou@gmail.com>
2026-07-06 12:38:59 -0400

    Minor update for readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import { BlockRenderer } from '../../components/BlockRenderer';
 import { mockDashboard } from '../../lib/mockData';
 
 export default function Home() {
+  const { title, blocks } = mockDashboard;
   return (
     <div>
-      <h1>{mockDashboard.title}</h1>
-      {mockDashboard.blocks.map((block) => (
+      <h1>{title}</h1>
+      {blocks.map((block) => (
         <BlockRenderer key={block.id} block={block} />
       ))}
     </div>
